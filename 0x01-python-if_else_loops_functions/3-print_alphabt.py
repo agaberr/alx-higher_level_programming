16d691f84eb4c1d309eec8783902296eceb42f9e
Ahmed Gaber <ahmedgaber312001@gmail.com>
2024-01-02 23:31:36 +0200

    tasks

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for alphabet in range('a', 'z' + 1):
-    if alphabet != 'q' and alphabet != 'e':
+for alphabet in range(97, 122 + 1):
+    if alphabet != 101 and alphabet != 113:
         print("{}".format(chr(alphabet)), end="")
