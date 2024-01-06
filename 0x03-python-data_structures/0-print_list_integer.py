9ca1b394bddf51cb49c0ac976a5614ef0a151653
Ahmed Gaber <ahmedgaber312001@gmail.com>
2024-01-06 22:28:44 +0200

    tasks

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+    """
+    Print integers in list
+    ...
+
+    Parameters
+    ----------
+    my_list : 
+        List of integers
+
+    Return:
+        None
+    """
     for num in my_list:
-        print("{}".format(num))
+        print("{:d}".format(num))
