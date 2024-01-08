436d5250c562e0e41db67fa112df59ade16c1ee4
Ahmed Gaber <ahmedgaber312001@gmail.com>
2024-01-08 17:05:25 +0200

    tasks

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        num = ' '.join(map(str,row))
-        print('{}'.format(num))
+        print(" ".join("{:d}".format(num) for num in row))
