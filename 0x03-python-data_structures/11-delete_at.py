d1193ef8d4595b0d31ce321475807b4260035012
Ahmed Gaber <ahmedgaber312001@gmail.com>
2024-01-08 17:10:59 +0200

    tasks

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,22 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    """
-    delete the item at a specific position in a list
-    ...
-
-    Parameters
-    ----------
-    my_list: 
-        List of integers
-    idx:
-        index where number should be deleted
-        
-    Return:
-        new_list after number is deleted
-    """
-
     if idx >= len(my_list) or idx < 0:
         return my_list
-    
     del my_list[idx]
     return my_list
