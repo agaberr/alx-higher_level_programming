6f4abbb0d9e39c4c21a1d67be5bb767ac1f01e62
Ahmed Gaber <ahmedgaber312001@gmail.com>
2024-01-08 17:08:16 +0200

    tasks

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,20 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    """
-    returns a tuple with the length of a
-    string and its first character
-    ...
-
-    Parameters
-    ----------
-    sentence : 
-        input string
-
-    Return:
-        new_string
-    """
-
     if len(sentence) == 0:
         return (0, None)
-    
     return (len(sentence), sentence[0])
