9f7e743bf498b2a2e1471a179eaebddfd14233f7
Ahmed Gaber <ahmedgaber312001@gmail.com>
2024-01-08 16:55:38 +0200

    tasks

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,22 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-
-    """
-     remove all characters c and C from a string
-    ...
-
-    Parameters
-    ----------
-    my_string : 
-        input string
-
-    Return:
-        new_string
-    """
-
     new_string = ""
     for c in my_string:
         if c not in "cC":
             new_string += c
-
     return new_string
