8fc58623d640732d6bd20e70f60b096eff966bdc
Ahmed Gaber <ahmedgaber312001@gmail.com>
2024-01-08 16:57:34 +0200

    tasks

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,20 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-
-    """
-    Prints a matrix of integers
-    ...
-
-    Parameters
-    ----------
-    matrix : 
-        matrix of integers
-
-    Return:
-        None
-    """
-
     for row in matrix:
-        for col in row:
-            print("{:d} ".format(col), end='')
-        print()
+        num = ' '.join(map(str,row))
+        print('{}'.format(num))
