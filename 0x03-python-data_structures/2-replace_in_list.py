61fdbf54258bab340ee990e0aab9fdfbee893153
Ahmed Gaber <ahmedgaber312001@gmail.com>
2024-01-08 16:49:09 +0200

    tasks

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,26 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-
-    """
-    replace an element of a list at a specific position
-    ...
-
-    Parameters
-    ----------
-    my_list: 
-        List of integers
-    idx:
-        index of element to retrieve
-    element:
-        element to replace
-
-    Return:
-        my_list
-    """
-
     if idx >= len(my_list) or idx < 0:
         return my_list
-
+    
     my_list[idx] = element
 
     return my_list
