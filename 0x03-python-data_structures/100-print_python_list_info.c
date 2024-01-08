e0bb3368870e1a414cdc27feb9cb3d3967526a02
Ahmed Gaber <ahmedgaber312001@gmail.com>
2024-01-08 19:55:10 +0200

    tasks

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,26 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info - prints info about python list
+ * @p: pointer represents pyhton list
+ */
+void print_python_list_info(PyObject *p)
+{
+
+	Py_ssize_t size, alloc, i;
+	PyObject *item;
+
+	size = PyList_Size(p);
+	alloc = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", alloc);
+
+
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+	}
+
+}
