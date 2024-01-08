926fa783a334d8434307f4e5f93e71e3f2d22f13
Ahmed Gaber <ahmedgaber312001@gmail.com>
2024-01-08 17:09:09 +0200

    tasks

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,24 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    """
-    find the biggest integer of a list
-    ...
-
-    Parameters
-    ----------
-    my_list: 
-        List of integers
-
-    Return:
-        max element in list, otherwise return None
-    """
-
     if len(my_list) <= 0:
         return None
-    
     max = -999
     for num in my_list:
         if num > max:
             max = num
-
     return max
