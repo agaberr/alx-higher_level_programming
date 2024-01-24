a9822456f7aade3e60698f8664962d806e8858bf
Ahmed Gaber <ahmedgaber312001@gmail.com>
2024-01-24 23:29:16 +0200

    tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""square module"""
+
+
+class Square:
+    """define square"""
+    
+    def __init__(self, size):
+        """Constructor
+        
+        Args:
+            size: length of square side
+        """
+        self.__size = size
