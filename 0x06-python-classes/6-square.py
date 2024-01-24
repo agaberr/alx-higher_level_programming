92d5909771024159a7ff7757bd8b748dbf8a4ada
Ahmed Gaber <ahmedgaber312001@gmail.com>
2024-01-24 23:57:31 +0200

    tasks

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -58,6 +58,10 @@ class Square:
 
         if self.__size == 0:
             print()
-        else:
-            for i in range (self.__size):
-                print("#" * self.__size)
+            return
+
+        [print() for i in range(self.__position[1])]
+        for i in range(self.__size):
+            [print(" ", end="") for j in range(self.__position[0])]
+            [print("#", end="") for k in range(self.__size)]
+            print()
