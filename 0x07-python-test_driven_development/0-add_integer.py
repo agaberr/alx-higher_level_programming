c2366f7ebaac824ef257fdc135ae1090c3e06e15
Ahmed Gaber <ahmedgaber312001@gmail.com>
2024-01-27 18:20:46 +0200

    tasks

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" module for addition """
+
+def add_integer(a, b=98):
+    """
+        Add two numbers
+
+        Args:
+            a: first number
+            b: second number
+
+        Raises:
+            TypeError: if not integers or float
+        
+        Return:
+            a + b
+    """
+    if type(a) not in (int, float):
+        raise TypeError("a must be an integer")
+    elif type(b) not in (int, float):
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
+
+if __name__ == '__main__':
+        import doctest
+        doctest.testfile("tests/0-add_integer.txt")
