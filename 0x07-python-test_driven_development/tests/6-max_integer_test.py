ffe7a4138e8fd89f84c5a6dbf59752de58670c6c
agaberr <ahmedgaber312001@gmail.com>
2024-02-01 23:14:13 +0200

    tasks

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,7 +5,7 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
-class MaxInteger(unittest.TestCase):
+class TestMaxInteger(unittest.TestCase):
     """Test max_integer func"""
 
     def test_normal(self):
