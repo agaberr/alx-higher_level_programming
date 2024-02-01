cc0312226b526fd3eac8ac2bffdf75d1da9ff600
agaberr <ahmedgaber312001@gmail.com>
2024-02-01 23:12:20 +0200

    tasks

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,65 +1,61 @@
-#!/usr/bin/python3
-"""Unittest formaxInteger module"""
-
 import unittest
-max_integer = __import__('6-max_integer').max_integer
-
+from max_integer import max_integer
 
 class MaxInteger(unittest.TestCase):
     """Test max_integer func"""
 
-    def normal(self):
+    def test_normal(self):
         """regular test"""
         l = [1, 2, 3, 4, 5]
         result = max_integer(l)
         self.assertEqual(result, 5)
 
-    def not_int(self):
+    def test_not_int(self):
         """non int in list"""
         l = ["a", "b", 9]
         self.assertRaises(TypeError, max_integer, l)
 
-    def empty_list(self):
+    def test_empty_list(self):
         """Empty list"""
         l = []
         result = max_integer(l)
         self.assertEqual(result, None)
 
-    def negative_number(self):
+    def test_negative_number(self):
         """list of negative numbers"""
         l = [-2, -6, -1]
         result = max_integer(l)
         self.assertEqual(result, -1)
 
-    def float_number(self):
+    def test_float_number(self):
         """float number in list"""
-        l = [3, 4.5, 2]
+        l = [3, 4, 2.5]
         result = max_integer(l)
-        self.assertEqual(result, 4.5)
+        self.assertEqual(result, 4)
 
-    def not_list(self):
+    def test_not_list(self):
         """list is not of type list"""
         self.assertRaises(TypeError, max_integer, 7)
 
-    def one_val(self):
+    def test_one_val(self):
         """one value only in list"""
         l = [45]
         result = max_integer(l)
         self.assertEqual(result, 45)
 
-    def same_number(self):
+    def test_same_number(self):
         """same number in list"""
         l = [8, 8, 8, 8, 8]
         result = max_integer(l)
         self.assertEqual(result, 8)
 
-    def string(self):
+    def test_string(self):
         """string in list"""
         l = ["hi", "hello"]
         result = max_integer(l)
-        self.assertEqual(result, "hi")
+        self.assertEqual(result, None)
 
-    def none(self):
+    def test_none(self):
         """list is None"""
         self.assertRaises(TypeError, max_integer, None)
 
