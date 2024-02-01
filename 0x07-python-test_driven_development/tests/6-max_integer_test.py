34ccfe72bbc0d551a2b713989cf19968d7c924a5
agaberr <ahmedgaber312001@gmail.com>
2024-02-01 23:13:05 +0200

    tasks

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,5 +1,9 @@
+#!/usr/bin/python3
+"""Unittest formaxInteger module"""
+
 import unittest
-from max_integer import max_integer
+max_integer = __import__('6-max_integer').max_integer
+
 
 class MaxInteger(unittest.TestCase):
     """Test max_integer func"""
