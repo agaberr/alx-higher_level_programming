5d4dc949fea12fa5776bc73c4c23ed71abfc1d97
agaberr <ahmedgaber312001@gmail.com>
2024-02-01 22:37:35 +0200

    tasks

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" module for printing name """
+
+def say_my_name(first_name, last_name=""):
+    """
+        function that prints My name is <first name> <last name>
+
+        Args:
+            first_name
+            last_name
+
+        Raises:
+            TypeError: if first and last names are not string
+        
+        Return:
+            None
+    """
+    if not isinstance(first_name, str) or not isinstance(first_name, str):
+        raise TypeError('first_name must be a string or last_name must be a string')
+    else:
+        print('My name is {} {}'.format(first_name, last_name))
+    
+if __name__ == '__main__':
+        import doctest
+        doctest.testfile("tests/2-matrix_divided.txt")
