3b84b45f24b3b575877934d2bd8b5ec80e4fdf79
agaberr <ahmedgaber312001@gmail.com>
2024-02-05 19:00:11 +0200

    tasks

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,7 +9,9 @@ Module with the class MyList
 class MyList(list):
 
     """Class My list inherits from list"""
+    pass
 
     def print_sorted(self):
         """print sorted list in ascending order"""
+
         print(sorted(self))
