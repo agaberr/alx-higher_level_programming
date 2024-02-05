83b3e3b5946faf32bd9d4cca37398ea4220eb6d3
agaberr <ahmedgaber312001@gmail.com>
2024-02-05 18:54:15 +0200

    tasks

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -9,4 +9,4 @@ Module with the method lookup
 def lookup(obj):
     """function that returns the list of available attributes and methods of an object:"""
 
-    return (dir(obj))
+    return dir(obj)
