3939d4fb476286eb33e3ee1a008e057c24e20886
agaberr <ahmedgaber312001@gmail.com>
2024-02-05 19:26:39 +0200

    tasks

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+=============================
+Module with the method inherits_from
+=============================
+"""
+
+class BaseGeometry():
+    """class BaseGeometry"""
+
+    pass
