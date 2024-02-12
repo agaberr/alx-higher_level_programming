dd123d125badc417b0afc2ef09667a2fd1eba66a
agaberr <ahmedgaber312001@gmail.com>
2024-02-12 20:44:17 +0200

    tasks

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -94,3 +94,10 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         else:
             self.__y = val
+
+    def area(self):
+        """
+        return area of rectangle
+        """
+
+        return self.__width * self.__height
