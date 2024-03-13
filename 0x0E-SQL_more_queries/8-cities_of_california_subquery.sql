1e887685c8d2d7dcf7fe96cb3eeedbb6dcbb2175
agaberr <ahmedgaber312001>
2024-03-13 13:59:06 +0000

    mandatory tasks

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,13 @@
+-- list all the cities of califonia found in hbtn_0d_usa
+SELECT `id`, `name`
+FROM `cities`
+WHERE `state_id` IN
+(
+SELECT `id`
+FROM `states`
+WHERE `name` = "California"
+)
+
+ORDER BY `id`
+
+);
