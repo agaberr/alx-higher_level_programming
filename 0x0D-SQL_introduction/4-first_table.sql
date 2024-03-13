7828927de5dffc8dc4300ebc87997fc5cdc67819
agaberr <ahmedgaber312001>
2024-03-13 10:43:57 +0000

    mandatory tasks

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,7 @@
-create table if not exists first_table{
+-- create table first_table with id and name 
+CREATE TABLE IF NOT EXISTS `first_table`(
 
-id INT;
-name VARCHAR(256);
+id INT,
+name VARCHAR(256)
 
-}
+);
