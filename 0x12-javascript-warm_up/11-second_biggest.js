5b1351ce83e70267fdc51bc70666e92e43beec6f
Ahmed Gaber <ahmedgaber312001@gmail.com>
2024-04-11 06:36:59 +0200

    test 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const sorted_list = process.argv.sort();
-  console.log(sorted_list.reverse()[3]);
+  const list = process.argv.sort();
+  console.log(list.reverse()[1]);
 }
